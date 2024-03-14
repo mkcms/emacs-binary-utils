1f151c7ec792587444095c49c5bfb5641de71432
Michał Krzywkowski <k.michal@zoho.com>
2024-03-14 12:54:18 +0100

    Add an ELPA package file
    * emacs-binary-utils.el: New file.

---
+++ emacs-binary-utils.el
@@ -0,0 +1,36 @@
+;;; emacs-binary-utils.el ---      -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024  Michał Krzywkowski
+
+;; Author: Michał Krzywkowski <k.michal@zoho.com>
+;; Keywords: languages, tools
+;; Package-Requires: ((emacs "26"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;; 
+
+;;; Code:
+
+(require 'asm-data)
+(require 'asm-jump)
+(require 'asm-x86)
+(require 'binfile)
+(require 'compdb)
+(require 'compiled-file)
+(require 'objdump)
+
+(provide 'emacs-binary-utils)
+;;; emacs-binary-utils.el ends here
