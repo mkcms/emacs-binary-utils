9a2c7f1bc192d683e6a5d5614283aaa832e3c370
Michał Krzywkowski <k.michal@zoho.com>
2024-12-28 12:11:35 +0100

    binfile: Show the binary file in a comment instead of a directive
    * binfile.el (binfile--disassemble): Put the binary filename in a
    comment instead.
    * binfile-test.el (binfile-disassembly):
    (binfile-disassembly-c++): Update tests.

--- binfile-test.el
+++ binfile-test.el
@@ -87,7 +87,7 @@ int foo(int x) { return x + 1; }
     (binfile-disassemble "foo" "file.o")
     (with-current-buffer binfile-disassembly-buffer
       (goto-char (point-min))
-      (should (search-forward (format ".file %S" (expand-file-name "file.o"))))
+      (should (search-forward (format "; file %S" (expand-file-name "file.o"))))
       (should (search-forward ".section .text"))
       (should (search-forward "foo:"))
       (should (search-forward "ret")))))
@@ -101,7 +101,7 @@ int foo(int x) { return x + 1; }
     (binfile-disassemble "_Z3fooi" "file.o")
     (with-current-buffer binfile-disassembly-buffer
       (goto-char (point-min))
-      (should (search-forward (format ".file %S" (expand-file-name "file.o"))))
+      (should (search-forward (format "; file %S" (expand-file-name "file.o"))))
       (should (search-forward ".section .text"))
       (should (search-forward "foo(int):"))
       (should (search-forward "ret")))))
